4fa82dc368d7563e734bc92c5846fa2131a1794d
Keiya Watanabe <ohagi2149@gmail.com>
2023-04-16 16:18:18 +0900

    update: セミコロン削除

--- scripts/js/dots.js
+++ scripts/js/dots.js
@@ -1,10 +1,10 @@
 // @ts-check
 // Usage: node dots.js [command] [options]
-const fs = require("fs");
-const path = require("path");
-const yargs = require("yargs");
+const fs = require("fs")
+const path = require("path")
+const yargs = require("yargs")
 const USER_HOME =
-  process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+  process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"]
 
 const CONFIGS = {
   test: [
@@ -40,14 +40,14 @@ const CONFIGS = {
       dst: `${USER_HOME}/`,
     },
   ],
-};
+}
 
 /**
  * 設定ファイルをデプロイする
  * @param {string} name
  */
 function deploy(name) {
-  const configs = CONFIGS[name];
+  const configs = CONFIGS[name]
 
   configs.forEach(({ src, dst, fileNames }) => {
     const files = fileNames
@@ -61,42 +61,42 @@ function deploy(name) {
         ...file,
         srcExists: fs.existsSync(file.srcPath),
         dstExists: fs.existsSync(file.dstPath),
-      }));
+      }))
 
     // 存在チェック
     // コピー元が存在し、コピー先が存在しない場合はtrue
     if (files.every(({ srcExists, dstExists }) => srcExists && !dstExists)) {
       // 存在しない場合はシンボリックリンクを作成
       files.forEach(({ srcPath, dstPath }) => {
-        fs.symlinkSync(srcPath, dstPath);
-      });
+        fs.symlinkSync(srcPath, dstPath)
+      })
 
-      console.log(`${name} config deployed!`);
+      console.log(`${name} config deployed!`)
       files.forEach(({ dstPath }) => {
-        console.log(`filePath: ${dstPath}`);
-      });
+        console.log(`filePath: ${dstPath}`)
+      })
     } else {
       files.map(({ fileName, srcExists, srcPath, dstExists, dstPath }) => {
         // コピー元ファイルが存在しない場合はエラーを出力
         if (!srcExists) {
-          console.log(`The ${fileName} does not exists`);
-          console.log(`filePath: ${srcPath}`);
+          console.log(`The ${fileName} does not exists`)
+          console.log(`filePath: ${srcPath}`)
         }
         // コピー先ファイルが存在する場合はエラーを出力
         if (dstExists) {
-          console.log(`The ${fileName} is already exists`);
-          console.log(`filePath: ${dstPath}`);
+          console.log(`The ${fileName} is already exists`)
+          console.log(`filePath: ${dstPath}`)
         }
-      });
+      })
     }
-  });
+  })
 }
 /**
  * 設定ファイルを削除する
  * @param {string} name
  */
 function remove(name) {
-  const configs = CONFIGS[name];
+  const configs = CONFIGS[name]
   configs.forEach(({ dst, fileNames }) => {
     const files = fileNames
       .map((fileName) => ({
@@ -107,37 +107,37 @@ function remove(name) {
       .map((file) => ({
         ...file,
         dstExists: fs.existsSync(file.dstPath),
-      }));
+      }))
 
     // 全ての設定ファイルが存在するかをチェック
     if (files.every(({ dstExists }) => dstExists)) {
       // 存在する場合は削除
       files.forEach(({ dstPath }) => {
-        fs.unlinkSync(dstPath);
-      });
+        fs.unlinkSync(dstPath)
+      })
 
-      console.log(`${name} config removed!`);
+      console.log(`${name} config removed!`)
       files.forEach(({ dstPath }) => {
-        console.log(`filePath: ${dstPath}`);
-      });
+        console.log(`filePath: ${dstPath}`)
+      })
     } else {
       // 存在しない場合は何もしない
       files.forEach(({ fileName, dstExists, dstPath }) => {
         if (!dstExists) {
-          console.log(`${fileName} does not exists`);
-          console.log(``);
-          console.log(`filePath: ${dstPath}`);
+          console.log(`${fileName} does not exists`)
+          console.log(``)
+          console.log(`filePath: ${dstPath}`)
         }
-      });
+      })
     }
-  });
+  })
 }
 /**
  * 設定ファイルの状態を確認する
  * @param {string} name
  */
 function status(name) {
-  const configs = CONFIGS[name];
+  const configs = CONFIGS[name]
   configs.forEach(({ dst, fileNames }) => {
     // 設定ファイルが存在するかをチェック
     const files = fileNames
@@ -149,18 +149,18 @@ function status(name) {
       .map((file) => ({
         ...file,
         dstExists: fs.existsSync(file.dstPath),
-      }));
+      }))
 
     files.forEach(({ fileName, dstExists, dstPath }) => {
       if (dstExists) {
-        console.log(`${fileName} is exists`);
+        console.log(`${fileName} is exists`)
       } else {
-        console.log(`${fileName} does not exist`);
+        console.log(`${fileName} does not exist`)
       }
-      console.log(`filePath: ${dstPath}`);
-      console.log(``);
-    });
-  });
+      console.log(`filePath: ${dstPath}`)
+      console.log(``)
+    })
+  })
 }
 
 yargs
@@ -171,11 +171,11 @@ yargs
       yargs.positional("name", {
         describe: "application name",
         type: "string",
-      });
+      })
     },
     (argv) => {
       // @ts-ignore
-      deploy(argv.name);
+      deploy(argv.name)
     }
   )
   .command(
@@ -185,11 +185,11 @@ yargs
       yargs.positional("name", {
         describe: "application name",
         type: "string",
-      });
+      })
     },
     (argv) => {
       // @ts-ignore
-      remove(argv.name);
+      remove(argv.name)
     }
   )
   .command(
@@ -199,13 +199,13 @@ yargs
       yargs.positional("name", {
         describe: "application name",
         type: "string",
-      });
+      })
     },
     (argv) => {
       // @ts-ignore
-      status(argv.name);
+      status(argv.name)
     }
   )
   .demandCommand(1, "At least one command is required")
   .strict()
-  .parse();
+  .parse()
